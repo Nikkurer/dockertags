0ff2e060f74f177938481d0ea4074b2d5b81efd3
Aleksey Trifonov <8029506+Nikkurer@users.noreply.github.com>
2020-05-13 12:21:19 +0300

    Initital commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# dockertags 
+`dockertags` get docker image tags from [Docker Hub](https://hub.docker.com) uses API v1.
+
+# Usage
+```bash
+$ dockertags -h
+    usage: dockertags.py [-h] image
+    
+    Get docker image tags.
+    
+    positional arguments:
+      image       Image name
+    
+    optional arguments:
+      -h, --help  show this help message and exit
+```
---
+++ dockertags.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+import requests
+
+URL = 'https://registry.hub.docker.com/v1/repositories/{image_name}/tags'
+
+
+def get_arguments():
+    arg_parser = ArgumentParser(add_help=True, description='Get docker image tags.')
+    arg_parser.add_argument('image', help='Image name')
+    # arg_parser.add_argument('--auth', action='store', help='Username:Password')
+    return arg_parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = get_arguments()
+    response = requests.get(URL.format(image_name=args.image))
+    if response:
+        for tags in response.json():
+            print(tags['name'])
+    else:
+        print(f'Something gone wrong: {response.status_code}')
